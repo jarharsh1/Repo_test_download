c9defebfc3ca76190b2f1c027b262800c7064057
Harsh Raj <89882233+jarharsh1@users.noreply.github.com>
2025-05-05 10:00:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
+def check_pallenderome (num):
+       
+    original_num = num  # Storing the original number for comparison
+    rev = 0
+    
+    while num > 0:  # Loop runs until num is greater than 0
+        d = num % 10
+        rev = (rev * 10) + d
+        num = num // 10
+    
+    if rev == original_num:
+        print(f"{original_num} is a palindrome number.")
+    else:
+        print(f"{original_num} is not a palindrome number.")
+
 num = int(input("Enter any number: "))
-
-original_num = num  # Storing the original number for comparison
-rev = 0
-
-while num > 0:  # Loop runs until num is greater than 0
-    d = num % 10
-    rev = (rev * 10) + d
-    num = num // 10
-
-if rev == original_num:
-    print(f"{original_num} is a palindrome number.")
-else:
-    print(f"{original_num} is not a palindrome number.")
+check_pallendrome(num)
